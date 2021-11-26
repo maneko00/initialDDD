1f4ee83b590138380c8ea5043081e3e6c5b2641c
maneko00 <trueneko1@gmail.com>
2021-11-26 21:24:45 +0900

    initial commit

---
+++ OriginalDDD.WinForm/ViewModels/ViewModelBase.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace OriginalDDD.WinForm.ViewModels
+{
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected bool SetProperty<T>(ref T field,
+            T value, string property_name)
+        {
+            if (Equals(field, value))
+            {
+                return false;
+            }
+
+            field = value;
+            var handler = this.PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(property_name));
+            }
+
+            return true;
+        }
+
+        public virtual DateTime GetDateTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
---
+++ OriginalDDDTest.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("OriginalDDDTest.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("OriginalDDDTest.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cab19e24-d43f-49b4-99d2-ed4275bd8928")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
